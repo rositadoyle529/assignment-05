c3b647c85ffb9f601a5b31546b2309e1d5c7edf4
Rosita <windair357@gmail.com>
2017-05-03 16:33:09 -0400

    Assignment 5
    Tahdah!

---
+++ starter_code/js/script.js
@@ -0,0 +1,43 @@
+<!--//checking the script is ready-->
+// $.ready() {}
+<!--//gather the city name-
+var city = $("form").val();
+<!--//person clicks the update button-->
+
+$('submit-btn').submit(enter);
+
+function enter() {
+			$('body').css('background', 'red');
+		}
+
+			//if city = "NYC" || "New York" || "New York City" {
+				//$('body').css('background', 'url("../images/nyc.jpg';)
+
+			
+// // Sanfrancisco//-->
+// 	else if city = "San Francisco" || "SF" || "Bay Area"
+// 		{
+// 			$('body').css('background', 'url("../images/sf.jpg');
+// 		}	
+// // Los Angelos//-->
+// 	else if city = "Los Angeles" || "LA" || "LAX"
+// 				{
+// 			$('body').css('background', 'url("../images/la.jpg');
+// 				}
+// // Austin //-->
+// 	else if city = "Austin" || "ATX"{
+
+// 			$('body').css('background', 'url("../images/austin.jpg');
+// 					}
+// //Sydney//-->					
+// 	else if city = "Sydney" || "SYD"{
+
+// 			$('body').css('background', 'url("../images/sydney.jpg');
+// 				}
+
+// 	// Unavailible City//-->
+// 	else {
+// 			$('body').css('background', 'url("../images/citpix_skyline.jpg')
+// 		}
+// 	}
+
